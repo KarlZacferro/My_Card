4ee0c53de09abba50be929d44a6f5d8d85171964
Your Name <kbb25t@gmail.com>
2022-05-04 20:24:01 -0300

    Adição do Icone

--- lib/main.dart
+++ lib/main.dart
@@ -12,11 +12,48 @@ class MeuApp extends StatelessWidget{
   Widget build(BuildContext context){
     return MaterialApp(
     home: Scaffold(
-      backgroundColor: Color.fromARGB(255, 71, 69, 69),
+      backgroundColor: Color.fromARGB(255, 46, 46, 46),
       body: SafeArea(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[],
+          children: <Widget>[
+            CircleAvatar(
+              radius: 50.0,
+              backgroundImage:AssetImage('imagens/Pic.jpeg'),
+            ),
+            Text(
+              'Karlisson Ferreira',
+                style: TextStyle(
+                color: Colors.white,
+                fontSize: 25,
+                fontWeight: FontWeight.bold),
+            ),
+            Text(
+              'My first app in Flutter',
+              style: TextStyle(color: Colors.grey.shade900,
+              fontSize:15 
+              ),
+            ),
+            SizedBox(
+              height: 30,
+              width: 200,
+              child: Divider(
+                color: Colors.white
+                ),
+            ),
+            Card(
+              margin: EdgeInsets.symmetric(vertical: 10.0,horizontal: 25.0),
+              child: ListTile(
+                leading: Icon(
+                  Icons.phone,
+                  color: Colors.black,
+                ),
+                title: Text('40028922',
+                style: TextStyle(fontSize: 10),
+                ),
+              ),
+            )
+          ],
         ),
        ),
       ),
